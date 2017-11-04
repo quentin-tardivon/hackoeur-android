9ac3ceea069386443e97900820b8d1df7839834f
Quentin Tardivon <quentin.tardivon@gmail.com>
2017-11-04 12:00:05 +0000

    Splashscreen & icon

---
+++ app/src/main/java/com/tardivon/quentin/hackoeur/SplashActivity.kt
@@ -0,0 +1,16 @@
+package com.tardivon.quentin.hackoeur
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this, MainActivity::class.java))
+
+        finish()
+    }
+}
