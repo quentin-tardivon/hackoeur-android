4f083ff3184703e901234cac2572f44a2f7e0e20
Quentin Tardivon <quentin.tardivon@gmail.com>
2017-11-17 09:33:09 +0000

    feat(map): Add sample MapActivity with API key

--- app/src/main/java/com/tardivon/quentin/hackoeur/SplashActivity.kt
+++ app/src/main/java/com/tardivon/quentin/hackoeur/SplashActivity.kt
@@ -9,7 +9,7 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        startActivity(Intent(this, EventListActivity::class.java))
+        startActivity(Intent(this, MainActivity::class.java))
 
         finish()
     }
