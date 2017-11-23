f56b27e615ee35389275911ef136244c15d7bb50
Quentin Tardivon <quentin.tardivon@gmail.com>
2017-11-23 14:09:52 +0000

    feat(Simple SignIn):

--- app/src/main/java/com/tardivon/quentin/hackoeur/MainActivity.kt
+++ app/src/main/java/com/tardivon/quentin/hackoeur/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.tardivon.quentin.hackoeur
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var fbAuth = FirebaseAuth.getInstance()
+        var fbUser = fbAuth.currentUser
+
+        if (fbUser == null) {
+            startActivity(Intent(this, SignInActivity::class.java))
+        } else {
+            var userName = fbUser!!.displayName
+
+            if (fbUser!!.photoUrl != null) {
+                var photoUrl = fbUser!!.photoUrl.toString()
+            }
+        }
+
     }
+
 }
---
+++ app/src/main/java/com/tardivon/quentin/hackoeur/SignInActivity.kt
@@ -0,0 +1,75 @@
+package com.tardivon.quentin.hackoeur
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import android.content.Intent
+import android.view.View
+import com.google.android.gms.auth.api.Auth
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.*
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.firebase.auth.AuthResult
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.GoogleAuthProvider
+
+
+class SignInActivity : AppCompatActivity() {
+
+    var RC_SIGN_IN = 9001
+    var fbAuth: FirebaseAuth? = null
+    var mGoogleSignInClient: GoogleSignInClient? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+        fbAuth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            var result: GoogleSignInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if (result.isSuccess) {
+                // Google Sign-In was successful, authenticate with Firebase
+                var account: GoogleSignInAccount? = result.signInAccount
+                firebaseAuthWithGoogle(account as GoogleSignInAccount)
+            } else {
+                // Google Sign-In failed
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        fbAuth!!.signInWithCredential(credential)
+                .addOnCompleteListener(this, OnCompleteListener<AuthResult> { task ->
+
+                    // If sign in fails, display a message to the user. If sign in succeeds
+                    // the auth state listener will be notified and logic to handle the
+                    // signed in user can be handled in the listener.
+                    if (!task.isSuccessful) {
+                        Toast.makeText(this@SignInActivity, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+                    } else {
+                        startActivity(Intent(this@SignInActivity, MainActivity::class.java))
+                        finish()
+                    }
+                })
+    }
+
+    fun signIn(view: View) {
+        Toast.makeText(this@SignInActivity, "Click",
+                Toast.LENGTH_SHORT).show()
+        val signInIntent = mGoogleSignInClient!!.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+}
