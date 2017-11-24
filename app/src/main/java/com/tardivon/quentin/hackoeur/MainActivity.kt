eb1be374c0bf9bf2493c2d5ee65fd70445148f41
Quentin Tardivon <quentin.tardivon@gmail.com>
2017-11-24 10:53:18 +0000

    style(appbar): We can now add action to appbar

--- app/src/main/java/com/tardivon/quentin/hackoeur/MainActivity.kt
+++ app/src/main/java/com/tardivon/quentin/hackoeur/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tardivon.quentin.hackoeur
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
 import android.view.View
 import com.google.firebase.auth.FirebaseAuth
 
@@ -13,6 +14,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val myToolbar = findViewById<View>(R.id.my_toolbar) as Toolbar
+        setSupportActionBar(myToolbar)
         var fbAuth = FirebaseAuth.getInstance()
         var fbUser = fbAuth.currentUser
 
