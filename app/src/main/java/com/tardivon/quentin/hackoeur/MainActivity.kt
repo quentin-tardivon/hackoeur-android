719a05857ae0949f3b307b0916c85d11af8ff9f6
Quentin Tardivon <quentin.tardivon@gmail.com>
2017-12-01 09:41:01 +0000

    style(main_activity): Change style of main activity (+ gradle tweak)

--- app/src/main/java/com/tardivon/quentin/hackoeur/MainActivity.kt
+++ app/src/main/java/com/tardivon/quentin/hackoeur/MainActivity.kt
@@ -8,7 +8,6 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import com.google.firebase.auth.FirebaseAuth
-import android.view.MenuInflater
 
 
 
@@ -19,31 +18,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val myToolbar = findViewById<View>(R.id.my_toolbar) as Toolbar
-        setSupportActionBar(myToolbar)
         var fbAuth = FirebaseAuth.getInstance()
         var fbUser = fbAuth.currentUser
 
         if (fbUser == null) {
             startActivity(Intent(this, SignInActivity::class.java))
-        } else {
-            startActivity(Intent(this, MyEventsActivity::class.java))
         }
-
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.nav, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        when(item?.itemId) {
-            R.id.eventList-> startActivity(Intent(this, EventListActivity::class.java))
-            R.id.myEvents -> startActivity(Intent(this, MyEventsActivity::class.java))
-        }
-        return true
     }
 
     fun signOut(view: View) {
