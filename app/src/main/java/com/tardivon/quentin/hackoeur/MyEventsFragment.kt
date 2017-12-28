c08330c8ffb9a847283a788b6472306120d7b72c
Alaskanhike <luc.dupuis@unb.ca>
2017-12-28 16:00:30 -0400

    changed to On Information Window Click for opening to google maps

--- app/src/main/java/com/tardivon/quentin/hackoeur/MyEventsFragment.kt
+++ app/src/main/java/com/tardivon/quentin/hackoeur/MyEventsFragment.kt
@@ -17,6 +17,10 @@ import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.GoogleMap
 import android.R.attr.label
 import android.net.Uri
+import com.google.android.gms.location.places.Place
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener
+
+
 
 
 /**
@@ -37,6 +41,7 @@ class MyEventsFragment : Fragment(), OnMapReadyCallback {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         rootView = inflater!!.inflate(R.layout.fragment_my_events, container, false)
 
+        //code used to initiate the google map on the fragment
         mMapView = rootView!!.findViewById<MapView>(R.id.mapView) as MapView
         mMapView!!.onCreate(savedInstanceState)
         mMapView!!.onResume()
@@ -48,26 +53,32 @@ class MyEventsFragment : Fragment(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        //loop to create all map markers
+        //loop to create all map markers for event and set the on MapMarkerClick function
         for(i in lat.indices) {
-            val sydney = LatLng(lat[i], long[i])
-            mMap!!.addMarker(MarkerOptions().position(sydney).title(title[i]))
-            mMap!!.setOnMarkerClickListener { marker ->
-                val uriBegin = "geo:"+ lat[i].toString() + ","+ long[i].toString()
-                val query = lat[i].toString() + ","+ long[i].toString()+"("+title[i]+")"
-                val encodedQuery = Uri.encode(query)
-                val uriString = uriBegin + "?q=" + encodedQuery
-                val uri = Uri.parse(uriString)
-                val intent = Intent(android.content.Intent.ACTION_VIEW, uri)
-                startActivity(intent)
-                true
-            };
+            //create marker
+            val marker = LatLng(lat[i], long[i])
+            val googleMarker = mMap!!.addMarker(MarkerOptions().position(marker).title(title[i])) as Marker
+            mMap!!.setOnInfoWindowClickListener(OnInfoWindowClickListener { marker ->
+                //when clicking the information window for a mapmarker open location in google maps
+                     val uriBegin = "geo:"+ marker.position.latitude.toString() + ","+  marker.position.longitude.toString()
+                     val query =  marker.position.latitude.toString() + ","+  marker.position.longitude.toString()+"("+marker.title+")"
+                     val encodedQuery = Uri.encode(query)
+                     val uriString = uriBegin + "?q=" + encodedQuery
+                     val uri = Uri.parse(uriString)
+                     val intent = Intent(android.content.Intent.ACTION_VIEW, uri)
+                     startActivity(intent)
+            })
         }
+
         val startinglocation = LatLng(53.348, -6.262) //hard coded location to be in Dublin
-        mMap!!.animateCamera(CameraUpdateFactory.newLatLngZoom(startinglocation,12.0f));
+        mMap!!.animateCamera(CameraUpdateFactory.newLatLngZoom(startinglocation,12.0f)); //set the zoom function to be in dublin
+
+        //Populate the list of event with the data stored in the arrays
         val adapter = MyEventsAdapter(this.activity ,title,picture, lat, long, mMap as GoogleMap)
         val lv = rootView!!.findViewById<ListView>(R.id.MyEventList) as ListView
         lv.adapter = adapter;
+
+        //add a onClickListener to the list
         lv.setClickable(true)
         lv.onItemClickListener = AdapterView.OnItemClickListener { adapter, activity_main, i, l ->
             Toast.makeText(this.context,"Position Clicked:"+" "+i, Toast.LENGTH_SHORT).show()
