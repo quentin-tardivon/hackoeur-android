26b4a04615e9257b623a6aad4b0bbe5f82796df3
Alaskanhike <luc.dupuis@unb.ca>
2018-01-03 13:15:11 -0400

    missed a xml file when removing old code

--- app/src/main/java/com/tardivon/quentin/hackoeur/EventDescriptionActivity.kt
+++ app/src/main/java/com/tardivon/quentin/hackoeur/EventDescriptionActivity.kt
@@ -2,13 +2,13 @@ package com.tardivon.quentin.hackoeur
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_event_description2.*
+import kotlinx.android.synthetic.main.activity_event_description.*
 
 class EventDescriptionActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_event_description2)
+        setContentView(R.layout.activity_event_description)
         val event = intent.getParcelableExtra<Event>("event")
         this.eventDate.text = event.date
         this.eventName.text = event.name
