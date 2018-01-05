397f18cca70bea0c9774a40d666a9ed8a5f41198
Alaskanhike <luc.dupuis@unb.ca>
2018-01-05 17:42:59 -0400

    Added the Subscribe functionality to the Event Description

--- app/src/main/java/com/tardivon/quentin/hackoeur/EventDescriptionActivity.kt
+++ app/src/main/java/com/tardivon/quentin/hackoeur/EventDescriptionActivity.kt
@@ -1,8 +1,11 @@
 package com.tardivon.quentin.hackoeur
 
+import android.content.Context
 import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.EventLog
+import android.widget.Toast
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -12,6 +15,8 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.tasks.OnFailureListener
 import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 import com.google.firebase.storage.FileDownloadTask
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.event_item.view.*
@@ -23,14 +28,18 @@ class EventDescriptionActivity : AppCompatActivity(), OnMapReadyCallback {
     private var event: Event? = null
     private var lat: Double? = null
     private var lng: Double? = null
+    private var eventKey: String? =null
+    private var user_uid : String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         val storageRef = FirebaseStorage.getInstance().getReference()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event_description)
+        user_uid = FirebaseAuth.getInstance().currentUser!!.uid
         event = intent.getParcelableExtra<Event>("event")
         lat = intent.getDoubleExtra("lat",0.0)
         lng = intent.getDoubleExtra("lng",0.0)
-
+        eventKey = intent.getStringExtra("key")
         this.eventDate.text = event!!.date
         this.eventName.text = event!!.name
         this.eventLocation.text = event!!.location
@@ -47,10 +56,72 @@ class EventDescriptionActivity : AppCompatActivity(), OnMapReadyCallback {
             })
 
         }
-
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.eventMap) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        setParticipateButtonText() // call the function to set the text of the participate button
+        // Set the OnClick Listener for the button for the subscribe / unsubscribe function
+        this.participateButton.setOnClickListener {
+            // We must first get and update the event to either subscribe(add the user id to the event) or unsubscribe(remove the user id from the event)
+            FirebaseDatabase.getInstance().getReference("Events").child(eventKey).addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(error: DatabaseError?) {
+                    println("Error")
+                }
+
+                override fun onDataChange(snapshot: DataSnapshot?) {
+                    val event = snapshot!!.getValue(Event::class.java)
+                    val NullCheck = event!!.registeredUsers.toString() as String?
+                    if (NullCheck == "null")
+                    {
+                        var EmptyList = mutableListOf<String>()
+                        EmptyList.add(user_uid!!)
+                        event!!.registeredUsers = EmptyList
+                        updateEvent(event)
+                        participateButton.text = "UnSubscribe"
+                    }
+                    else
+                    {
+                        if (event!!.registeredUsers!!.contains(user_uid!!)) {
+                            event!!.registeredUsers!!.remove(user_uid!!)
+                            updateEvent(event)
+                            participateButton.text = "Subscribe"
+                        } else {
+                            event!!.registeredUsers!!.add(user_uid!!)
+                            updateEvent(event)
+                            participateButton.text = "UnSubscribe"
+                        }
+                    }
+                }
+            })
+
+            // We must now get and update the user to either subscribe(add the event id to the user) or unsubscribe(remove the event id from the user)
+            FirebaseDatabase.getInstance().getReference("Users").child(user_uid).addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(error: DatabaseError?) {
+                    println("Error")
+                }
+                override fun onDataChange(snapshot: DataSnapshot?) {
+                    val user = snapshot!!.getValue(User::class.java)
+                    val NullCheck = user!!.eventList.toString() as String?
+                    if (NullCheck == "null")
+                    {
+                        var EmptyList = mutableListOf<String>()
+                        EmptyList.add(eventKey!!)
+                        user!!.eventList = EmptyList
+                        updateUser(user)
+                    }
+                    else
+                    {
+                        if (user!!.eventList!!.contains(eventKey!!)) {
+                            user!!.eventList!!.remove(eventKey!!)
+                            updateUser(user)
+                        } else {
+                            user!!.eventList!!.add(eventKey!!)
+                            updateUser(user)
+                        }
+                    }
+                }
+            })
+        } // end of the button onclick
     }
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
@@ -59,5 +130,42 @@ class EventDescriptionActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap!!.addMarker(MarkerOptions().position(marker).title(event!!.name))
         mMap!!.animateCamera(CameraUpdateFactory.newLatLngZoom(marker, 12.0f));
 
+    }
+
+    fun updateEvent(event : Event)
+    {
+        FirebaseDatabase.getInstance().getReference("Events").child(eventKey).setValue(event!!)
+    }
+
+    fun updateUser(user : User)
+    {
+        FirebaseDatabase.getInstance().getReference("Users").child(user_uid).setValue(user!!)
+    }
+    fun setParticipateButtonText()
+    {
+        FirebaseDatabase.getInstance().getReference("Events").child(eventKey).addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onCancelled(error: DatabaseError?) {
+                println("Error")
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot?) {
+                val event = snapshot!!.getValue(Event::class.java)
+                val NullCheck = event!!.registeredUsers.toString() as String?
+                if (NullCheck == "null") {
+                    participateButton.text = "Subscribe"
+                }
+                else
+                {
+                    if (event!!.registeredUsers!!.contains(user_uid!!))
+                    {
+                        participateButton.text = "UnSubscribe"
+                    }
+                    else
+                    {
+                        participateButton.text = "Subscribe"
+                    }
+                }
+            }
+        })
     }
 }
